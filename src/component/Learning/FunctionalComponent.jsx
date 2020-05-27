fea507cb2071f1f6ca607892ac68e04b61d6b870
Kumar Santosh <kumar@Admins-MacBook-Pro-3.local>
2020-05-27 21:11:00 +0530

    added components of counter-ui

---
+++ src/component/Learning/FunctionalComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FourthComponent() {
+    return (
+        <div className = "fourthComponent">
+            FourthComponent
+        </div>
+    )
+}
+
+export default FourthComponent;
---
+++ src/component/Learning/ThirdComponent.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class ThirdComponent extends Component {
+    render(){
+      return (
+        <div className = "thirdComponent">
+          ThirdComponent
+           </div>
+      )
+    }
+  }
+
+  export default ThirdComponent
